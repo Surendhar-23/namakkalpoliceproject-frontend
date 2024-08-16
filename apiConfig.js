c345c21cdf356fd06951675f024f5550edaeab54
SURENDHAR N D <ndsurendhar2004@gmail.com>
2024-08-17 02:04:47 +0530

    api added

--- apiConfig.js
+++ apiConfig.js
@@ -1,3 +1,4 @@
 // const API_BASE_URL = "http://localhost:3000/api";
-const API_BASE_URL = "http://192.168.42.138:3000/api";
+const API_BASE_URL =
+  "https://namakkalpoliceproject-backend-q52v1lvws-surendhar23s-projects.vercel.app/api";
 export default API_BASE_URL;
